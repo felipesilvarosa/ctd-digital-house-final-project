35d381d716da1cb02b8dffd34cea959a87aa18d0
matandrade <matheus.andrade@capgemini.com>
2022-03-18 20:56:16 -0300

    Added reservation api. Finished sprint 3

---
+++ backend/src/main/java/com/grupo01/digitalbooking/controller/ReservationController.java
@@ -0,0 +1,47 @@
+package com.grupo01.digitalbooking.controller;
+
+import com.grupo01.digitalbooking.dto.ReservationDTO;
+import com.grupo01.digitalbooking.dto.DefaultResponseDTO;
+import com.grupo01.digitalbooking.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+import static com.grupo01.digitalbooking.dto.DefaultResponseDTO.Status.SUCCESS;
+
+@RestController
+@RequestMapping("reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @GetMapping()
+    public ResponseEntity<DefaultResponseDTO> findAllReservations() {
+        List<ReservationDTO> response = reservationService.getReservations();
+        Map<String, List<ReservationDTO>> data = Map.of("reservation", response);
+        return ResponseEntity.ok(new DefaultResponseDTO(SUCCESS, data, "Reservations retrieved successfully"));
+    }g
+
+    @PostMapping("/new")
+    public ResponseEntity<DefaultResponseDTO> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        ReservationDTO response = reservationService.createReservation(reservationDTO);
+        Map<String, ReservationDTO> data = Map.of("reservation", response);
+        return ResponseEntity.ok(new DefaultResponseDTO(SUCCESS, data, "Reservation created successfully"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<DefaultResponseDTO> deleteReservation(@PathVariable Long id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.ok(new DefaultResponseDTO(SUCCESS, "Reservation deleted successfully"));
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<DefaultResponseDTO> editReservation(@RequestBody ReservationDTO reservationDTO) {
+        ReservationDTO response = reservationService.editReservation(reservationDTO);
+        Map<String, ReservationDTO> data = Map.of("reservation", response);
+        return ResponseEntity.ok(new DefaultResponseDTO(SUCCESS, data, "Reservation edited successfully"));
+    }
+}
