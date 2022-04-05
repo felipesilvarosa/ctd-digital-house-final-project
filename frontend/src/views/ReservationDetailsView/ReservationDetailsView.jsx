bf190d807a07cdcb373e6dff500dbea7834a47eb
mdaffonso <mdaffonso@gmail.com>
2022-04-05 15:57:46 -0300

    Ajustes na tela de reserva

--- frontend/src/views/ReservationDetailsView/ReservationDetailsView.jsx
+++ frontend/src/views/ReservationDetailsView/ReservationDetailsView.jsx
@@ -47,75 +47,83 @@ export const ReservationDetailsView = () => {
   return (
     <>
       <SpacingShim height="4.75rem" />
-      <ResponsiveContainer className={styles.ReservationDetails}>
-        <BackButton to={"/products/" + params.id}>Voltar</BackButton>
-        {
-          loadingProduct || loadingReservation || !reservation.product ?
-            <h1>Carregando...</h1>
-          :
-            <>
-              <h1 className={styles.ReservationTitle}>{reservation.product.title}</h1>
-              <section className={styles.Reservation}>
-                <div className={styles.ReservationBody}>
-                  <div className={styles.ReservationBlock}>
-                    <h2>Complete seus dados</h2>
-                    <div className={styles.ReservationCard}>
-                      <p>
-                        <strong>Nome</strong>
-                        <span>{reservation.user.fullName}</span>
-                      </p>
-                      <p>
-                        <strong>E-mail</strong>
-                        <span>{reservation.user.email}</span>
-                      </p>
+      <div>
+        <ResponsiveContainer className={styles.ReservationDetails}>
+          <BackButton to={"/products/" + params.id}>Voltar</BackButton>
+          {
+            loadingProduct || loadingReservation || !reservation.product ?
+              <h1>Carregando...</h1>
+            :
+              <>
+                <h1 className={styles.ReservationTitle}>{reservation.product.title}</h1>
+                <section className={styles.Reservation}>
+                  <div className={styles.ReservationBody}>
+                    <div className={styles.ReservationBlock}>
+                      <h2>Complete seus dados</h2>
+                      <div className={styles.ReservationCard}>
+                        <p>
+                          <strong>Nome</strong>
+                          <span>{reservation.user.fullName}</span>
+                        </p>
+                        <p>
+                          <strong>E-mail</strong>
+                          <span>{reservation.user.email}</span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className={styles.ReservationBlock}>
+                      <h2>Selecione sua data de reserva</h2>
+                      <div className={`${styles.ReservationCard} ReservationDetailsCalendar`}>
+                        <DateRange 
+                          direction={size.width > 1299 ? "horizontal" : "vertical"}
+                          weekdayDisplayFormat={size.width > 800 ? "E" : "EEEEE"}  
+                          locale={pt}
+                          ranges={[ranges]}
+                          onChange={handleSelect}
+                          dateDisplayFormat="d/MMM/yyyy"
+                          months={2}
+                          minDate={new Date()}
+                        />
+                      </div>
+                    </div>
+                    <div className={styles.ReservationBlock}>
+                      <h2>Selecione seu horário de chegada</h2>
+                      <div className={styles.ReservationCard}>Horário</div>
                     </div>
                   </div>
-                  <div className={styles.ReservationBlock}>
-                    <h2>Selecione sua data de reserva</h2>
-                    <div className={`${styles.ReservationCard} ReservationDetailsCalendar`}>
-                      <DateRange 
-                        direction={size.width > 930 ? "horizontal" : "vertical"}
-                        weekdayDisplayFormat={size.width > 800 ? "E" : "EEEEE"}  
-                        locale={pt}
-                        ranges={[ranges]}
-                        onChange={handleSelect}
-                        dateDisplayFormat="d/MMM/yyyy"
-                        months={2}
-                        minDate={new Date()}
-                      />
+                  <div className={styles.ReservationAside}>
+                    <h2>Detalhes da reserva</h2>
+                    <img src={reservation.product.images[0]} />
+                    <div>
+                      <p className={styles.CategoryName}>Hotel</p>
+                      <h3>{reservation.product.title}</h3>
+                      {reservation.product.stars && <StarsMeter value={reservation.product.stars} />}
                     </div>
-                  </div>
-                  <div className={styles.ReservationBlock}>
-                    <h2>Selecione seu horário de chegada</h2>
-                    <div className={styles.ReservationCard}>Horário</div>
-                  </div>
-                </div>
-                <div className={styles.ReservationAside}>
-                  <h2>Detalhes da reserva</h2>
-                  <img src={reservation.product.images[0]} />
-                  <div>
-                    <p>Hotel</p>
-                    <h3>{reservation.product.title}</h3>
-                    {reservation.product.stars && <StarsMeter value={reservation.product.stars} />}
-                  </div>
-                  <p>{reservation.product.location}</p>
-                  <div className={styles.ReservationDates}>
-                    <p>
-                      <span>Check in</span>
-                      <span>23/11/2022</span>
+                    <p className={styles.Address}>
+                      <span className="material-icons">place</span>
+                      <span>
+                        {reservation.product.address}
+                      </span>
                     </p>
-                    <hr />
-                    <p>
-                      <span>Check out</span>
-                      <span>23/12/2022</span>
-                    </p>
+                    <div className={styles.ReservationDates}>
+                      <p>
+                        <span>Check in</span>
+                        <span>{ranges.startDate.toLocaleDateString("pt-BR")}</span>
+                      </p>
+                      <hr />
+                      <p>
+                        <span>Check out</span>
+                        <span>{ranges.endDate.toLocaleDateString("pt-BR")}</span>
+                      </p>
+                    </div>
+                    <BaseButton>Confirmar Reserva</BaseButton>
                   </div>
-                  <BaseButton>Reservar</BaseButton>
-                </div>
-              </section>
-            </>
-        }
-      </ResponsiveContainer>
+                </section>
+              </>
+          }
+        </ResponsiveContainer>
+        { reservation?.product?.policies && <ProductDetailsPolicies policies={reservation.product.policies} className={styles.Policies} title="O que você precisa saber" />}
+      </div>
     </>
   )
 }
